0614c0490b0baf68d00fe2c91e2634890ea77243
Luís Cipriani <cunhaluisg5@gmail.com>
2023-01-06 20:54:47 -0300

    Adiciona listagem de digimon na tela Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './home.css';
+import api from '../../services/api';
 
-const Home = () =>{
-    return(
+const Home = () => {
+    const [listaDigimon, setListaDigimon] = useState([]);
+
+    useEffect(() => {
+        const carregaDigimon = async () => {
+            await api.get('/')
+                .then((res) => {
+                    const lista = res.data;
+                    setListaDigimon(lista);
+                })
+                .catch((error) => {
+                    console.log('Erro ao buscar digimon: ', error);
+                });
+        }
+        carregaDigimon();
+    }, []);
+
+    const renderLista = () => {
+        return(
+            <div>
+                {listaDigimon.map((digimon, index) => {
+                    return <div key={index}>
+                        <span>{digimon.name} + ' , ' + {digimon.level}</span>
+                    </div>
+                })}
+            </div>
+        )
+    }
+
+    return (
         <div className='container'>
             <h1>Página Home</h1>
+            {listaDigimon.length > 0 && renderLista()}
         </div>
     )
 }
