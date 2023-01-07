5dea244e93c36b762e8e1dc81b00840ccfd7e122
Luís Cipriani <cunhaluisg5@gmail.com>
2023-01-06 22:43:04 -0300

    Adiciona paginação na listagem da página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { Box, List, ListItem, Divider } from '@chakra-ui/core';
+import { Pagination } from '@material-ui/lab';
 
 import './home.css';
 import api from '../../services/api';
 
 const Home = () => {
     const [listaDigimon, setListaDigimon] = useState([]);
+    const [pagina, setPagina] = useState(1);
+    const [paginaAtual, setPaginaAtual] = useState(1);
+    const [contador, setContador] = useState(0);
+    const [paginaMaxima, setPaginaMaxima] = useState(0);
+    const itensPorPagina = 10;
 
     useEffect(() => {
         const carregaDigimon = async () => {
@@ -12,6 +19,8 @@ const Home = () => {
                 .then((res) => {
                     const lista = res.data;
                     setListaDigimon(lista);
+                    setContador(Math.ceil(lista.length / itensPorPagina));
+                    setPaginaMaxima(Math.ceil(lista.length / itensPorPagina))
                 })
                 .catch((error) => {
                     console.log('Erro ao buscar digimon: ', error);
@@ -20,16 +29,54 @@ const Home = () => {
         carregaDigimon();
     }, []);
 
+    const handleChange = (e, p) => {
+        setPagina(p);
+        avancaPagina(p);
+    };
+
+    const listaAtual = () => {
+        const inicio = (paginaAtual - 1) * itensPorPagina;
+        const fim = inicio + itensPorPagina;
+        return listaDigimon.slice(inicio, fim);
+    }
+
+    const avancaPagina = (pagina) => {
+        const numeroPagina = Math.max(1, pagina);
+        setPaginaAtual(Math.min(numeroPagina, paginaMaxima));
+    }
+
     const renderLista = () => {
-        return(
-            <div>
-                {listaDigimon.map((digimon, index) => {
-                    return <div key={index}>
-                        <span>{digimon.name} + ' , ' + {digimon.level}</span>
-                    </div>
+        return <Box p='5'>
+            <Pagination
+                count={contador}
+                size='large'
+                page={pagina}
+                variant='outlined'
+                shape='rounded'
+                onChange={handleChange}
+            />
+            <List p='10' pt='3' spacing={2}>
+                {listaAtual().map((digimon, index) => {
+                    return (
+                        <ListItem key={index} listStyleType='disc'>
+                            <span>{digimon.name}</span>
+                            <Divider display='inline' orientation='vertical' />
+                            <span> {digimon.level}</span>
+                            <Divider display='inline' orientation='vertical' />
+                            <img src={digimon.img} alt={digimon.img} />
+                        </ListItem>
+                    );
                 })}
-            </div>
-        )
+            </List>
+            <Pagination
+                count={contador}
+                size='large'
+                page={pagina}
+                variant='outlined'
+                shape='rounded'
+                onChange={handleChange}
+            />
+        </Box>
     }
 
     return (
